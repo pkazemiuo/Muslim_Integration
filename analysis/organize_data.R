bac4a365482adba2b5d6b0f45d219886f53e7c12
Aaron Gullickson <aarong@uoregon.edu>
2019-05-15 10:58:15 -0700

    better code for reading in data

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -8,6 +8,6 @@
 
 #source in any useful functions
 source("useful_functions.R")
-setwd('~/soc312/Muslim_integration/analysis')
-library(readr)
-muslim_politics <- read.csv("input/pew_muslim_data.csv")
+
+library(haven)
+muslim_politics <- read_dta("input/Muslim-American-Final-Questionnaire/2017USMuslimPublicData - checked.dta")
