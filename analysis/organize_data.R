2da0d248f83ab8b925718093ce2224b8f356a5f9
Aaron Gullickson <aarong@uoregon.edu>
2019-05-16 10:46:34 -0700

    coded voting variable

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -11,3 +11,21 @@ source("useful_functions.R")
 
 library(haven)
 muslim_politics <- read_dta("input/Muslim-American-Final-Questionnaire/2017USMuslimPublicData - checked.dta")
+
+muslim_politics$vote <- factor(ifelse(muslim_politics$rega!=1 | muslim_politics$pvote16a>2, NA,
+                                      ifelse(muslim_politics$pvote16a==2, "No vote",
+                                             ifelse(muslim_politics$pvote16b==1, "Trump",
+                                                    ifelse(muslim_politics$pvote16b==2, "Clinton",
+                                                           ifelse(muslim_politics$pvote16b==3, "Third party", NA))))),
+                               levels=c("Clinton","Trump","Third party","No vote"))
+table(muslim_politics$pvote16b, muslim_politics$vote, exclude=NULL)
+table(muslim_politics$pvote16a, muslim_politics$vote, exclude=NULL)
+table(muslim_politics$rega, muslim_politics$vote, exclude=NULL)
+
+## a bunch of other stuff
+
+analytical_data <- subset(muslim_politics, !is.na(vote),
+                          select=c("vote"))
+
+save(analytical_data, file="output/analytical_data.RData")
+
